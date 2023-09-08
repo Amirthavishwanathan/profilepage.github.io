b9eb7693743f9165267acd7e4439e75c50e9fbd0
Amirthavishwanathan <113587597+Amirthavishwanathan@users.noreply.github.com>
2023-09-08 15:06:37 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,79 +1,44 @@
-<?php 
-session_start();
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+    	
+    	<form class="shadow w-450 p-3" 
+    	      action="php/login.php" 
+    	      method="post">
 
-if(isset($_POST['uname']) && 
-   isset($_POST['pass'])){
+    		<h4 class="display-4  fs-1">LOGIN</h4><br>
+    		<?php if(isset($_GET['error'])){ ?>
+    		<div class="alert alert-danger" role="alert">
+			  <?php echo $_GET['error']; ?>
+			</div>
+		    <?php } ?>
 
-    include "../db_conn.php";
+		  <div class="mb-3">
+		    <label class="form-label">User name</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="uname"
+		           value="<?php echo (isset($_GET['uname']))?$_GET['uname']:"" ?>">
+		  </div>
 
-    $uname = $_POST['uname'];
-    $pass = $_POST['pass'];
-
-    $data = "uname=".$uname;
-    
-    if(empty($uname)){
-    	$em = "User name is required";
-    	header("Location: ../login.php?error=$em&$data");
-	    exit;
-    }else if(empty($pass)){
-    	$em = "Password is required";
-    	header("Location: ../login.php?error=$em&$data");
-	    exit;
-    }else {
-
-    	$sql = "SELECT * FROM users WHERE username = ?";
-    	$stmt = $conn->prepare($sql);
-    	$stmt->execute([$uname]);
-
-      if($stmt->rowCount() == 3){
-          $user = $stmt->fetch();
-
-          $username =  $user['username'];
-          $password =  $user['password'];
-          $fname =  $user['fname'];
-          $email =  $user['email'];
-          $phone =  $user['phone'];
-          $ad =  $user['ad'];
-          $dob =  $user['dob'];
-          $id =  $user['id'];
-          $pp =  $user['pp'];
-
-          if($username === $uname){
-             if(password_verify($pass, $password)){
-                 $_SESSION['id'] = $id;
-                 $_SESSION['fname'] = $fname;
-                 $_SESSION['email'] = $email;
-                 $_SESSION['phone'] = $phone;
-                 $_SESSION['ad'] = $ad;
-                 $_SESSION['dob'] = $dob;
-                 $_SESSION['pp'] = $pp;
-                
-                 header("Location: ../home.php");
-                 exit;
-             }else {
-                $em = "invalid username";
-                header("Location: ../login.php?error=$em");
-                exit;
-               
-            }
-
-          }else {
-            $em = "invalid username";
-            header("Location: ../login.php?error=$em");
-            exit;
-           
-         }
-
-      }else {
-        $em = "invalid username";
-        header("Location: ../login.php?error=$em");
-        exit;
-         
-      }
-    }
-
-
-}else {
-	header("Location: ../login.php?error=error");
-	exit;
-}
+		  <div class="mb-3">
+		    <label class="form-label">Password</label>
+		    <input type="password" 
+		           class="form-control"
+		           name="pass">
+		  </div>
+		  
+		  <button type="submit" class="btn btn-primary">Login</button>
+		  <a href="index.php" class="link-secondary">Sign Up</a>
+		</form>
+    </div>
+</body>
+</html>
