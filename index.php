d3fc4a4d2684d8c58012c260a6c565675962f907
Amirthavishwanathan <113587597+Amirthavishwanathan@users.noreply.github.com>
2023-09-08 09:08:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sign Up</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.24/sweetalert2.all.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+<div class="flex-container">
+
+    <div class="d-flex justify-content-center align-items-center vh-100">
+    	
+    	<form class="shadow w-450 p-3" 
+    	      action="php/signup.php" 
+    	      method="post"
+    	      enctype="multipart/form-data">
+
+    		<h4 class="display-4  fs-1">Create Account</h4><br>
+    		<?php if(isset($_GET['error'])){ ?>
+    		<div class="alert alert-danger" role="alert">
+			  <?php echo $_GET['error']; ?>
+			</div>
+		    <?php } ?>
+
+		    <?php if(isset($_GET['success'])){ ?>
+    		<div class="alert alert-success" role="alert">
+			  <?php echo $_GET['success']; ?>
+			</div>
+		    <?php } ?>
+		  <div class="mb-3">
+		    <label class="form-label">Full Name</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="fname"
+		           value="<?php echo (isset($_GET['fname']))?$_GET['fname']:"" ?>">
+		  </div>
+
+		  <div class="mb-3">
+		    <label class="form-label">User name</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="uname"
+		           value="<?php echo (isset($_GET['uname']))?$_GET['uname']:"" ?>">
+		  </div>
+    
+		  <div class="mb-3">
+		    <label class="form-label">Email</label>
+		    <input type="email" 
+		           class="form-control"
+		           name="email"
+		           value="<?php echo (isset($_GET['email']))?$_GET['email']:"" ?>">
+		  </div>
+		  
+		  <div class="mb-3">
+		    <label class="form-label">Phone</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="phone"
+		           value="<?php echo (isset($_GET['phone']))?$_GET['phone']:"" ?>">
+		  </div>
+		  
+		  <div class="mb-3">
+		    <label class="form-label">address</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="ad"
+		           value="<?php echo (isset($_GET['ad']))?$_GET['ad']:"" ?>">
+		  </div>
+		  
+		  <div class="mb-3">
+		    <label class="form-label">Date of birth</label>
+		    <input type="date" 
+		           class="form-control"
+		           name="dob"
+		           value="<?php echo (isset($_GET['dob']))?$_GET['dob']:"" ?>">
+		  </div>
+		  
+		  <div class="mb-3">
+		    <label class="form-label">Password</label>
+		    <input type="password" 
+		           class="form-control"
+		           name="pass">
+		  </div>
+
+		  <div class="mb-3">
+		    <label class="form-label">Profile Picture</label>
+		    <input type="file" 
+		           class="form-control"
+		           name="pp">
+		  </div>
+		  
+		  <button type="submit" class="btn btn-primary">Sign Up</button>
+		  <a href="login.php" class="link-secondary">Login</a>
+		</form>
+			</div>
+    </div>
+			</div>
+</body>
+</html>
